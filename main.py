f23451e20fc489e1092411c99d5edb3b30df826d
André Jr <48335644+Andre3224@users.noreply.github.com>
2025-08-12 22:14:36 -0300

    Atualização
    Remake baseado totalmente na aula do professor

---
+++ main.py
@@ -0,0 +1,44 @@
+from flask import Flask, make_response
+from markupsafe import escape
+from flask import render_template
+from flask import request
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+@app.route("/cad/usuario")
+def usuario():
+    return render_template('usuario.html')
+
+@app.route("/cad/anuncio")
+def anuncio():
+    return render_template('anuncio.html')
+
+@app.route("/anuncio/pergunta")
+def pergunta():
+    return render_template('pergunta.html')
+
+@app.route("/anuncio/compra")
+def compra():
+    print("Anuncio Comprado")
+    return ""
+
+@app.route("/anuncio/favoritos")
+def favoritos():
+    print("Favorito Inserido")
+    return ""
+
+@app.route("/config/categoria")
+def categoria():
+    return render_template('categoria.html')
+
+@app.route("/relatorios/vendas")
+def relVendas():
+    return render_template('relVendas.html')
+
+@app.route("/relatorios/compras")
+def relCompras():
+    return render_template('relVCompras.html')
